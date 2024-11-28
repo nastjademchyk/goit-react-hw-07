a708c8867b8c8511d975874aaf6720c30c40fca8
Nastja Demchyk <NastjaDemchyk@gmail.com>
2024-11-28 13:48:40 +0100

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+import SearchBox from "./SearchBox/SearchBox";
+
+function App() {
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,31 @@
+import s from "./ContactList.module.css";
+import Contact from "../Contact/Contact";
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
+
+const ContactList = () => {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <ul className={s.list}>
+      {filteredContacts.map((contact) => (
+        <li className={s.item} key={contact.id}>
+          <Contact
+            name={contact.name}
+            number={contact.number}
+            id={contact.id}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
